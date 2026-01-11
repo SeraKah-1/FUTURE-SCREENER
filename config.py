100dab41fa2aeff8faa20261c3df5b1b730958cd
SeraKah-1 <farreladitya38@gmail.com>
2026-01-11 18:41:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ TF_MACRO = '1h'          # Trend Bias
 TF_MICRO = '15m'         # Entry Trigger
 
 # --- FILTER KUALITAS ---
-MIN_SCORE = 60           # Sinyal divalidasi jika skor > 55
+MIN_SCORE = 70           # Sinyal divalidasi jika skor > 55
                          # (Angka 55 adalah Sweet Spot: Tidak terlalu ketat, tidak terlalu longgar)
 
 # --- INDIKATOR ---
